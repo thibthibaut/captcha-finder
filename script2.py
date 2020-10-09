5ceb4260b453c70c34313a0bfa660284f664fc89
Thibaut Vercueil <thibo.v@gmail.com>
2020-10-09 15:03:14 +0200

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,9 @@
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# thibthibaut wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+# ----------------------------------------------------------------------------
 import requests
 from requests.exceptions import RequestException
 from contextlib import closing
